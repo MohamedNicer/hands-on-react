3f908b888bbd25f64e73461a06f5d90e18e63107
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-26 05:37:19 +0000

    Templating with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 function App() {
-  const name = 'StarGazers'
+  const someName = 'StarGazers'
   return (
     <div className="container">
       <article>
-      <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{color:"SteelBlue"}}>{name}</i></h1>
-        <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={()=> alert('Hi there')}>Click Me</button>
-      </hgroup>
+        <hgroup>
+          <h1>Meet the <i style={{ color: 'blueviolet' }}>{someName}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi There!')}>Click It</button>
+        </hgroup>
       </article>
     </div>
   )
