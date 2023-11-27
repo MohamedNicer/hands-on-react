21219921047d7333226e205fd6be745d81699b7a
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 16:55:55 +0000

    Building a Modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import ListCast from './components/ListCast';
 import Modals from './components/Modals';
 
 function App() {
-  const name = 'StarGazers'
-  let [memberInfo, setMemberInfo] = useState(null);
+  const [memberInfo , setMemberInfo] = useState(null);
   return (
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>Meet the Stargazers</h1>
+        <h1>Meet the <i style={{color:"blueviolet"}}>StarGazers</i></h1>
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
+        <ListCast onChoice={(info) => setMemberInfo(info)}/>
+        {memberInfo && <Modals member={memberInfo} handleClose={()=>{
+          setMemberInfo(null)
+        }}/> }
       </hgroup>
     </div>
   )
