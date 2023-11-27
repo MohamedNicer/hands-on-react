e572783696d3cc33ec17d39d64558c246dcffd48
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 05:10:07 +0000

    Classes or Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-
-class Welcome extends React.Component {
-  constructor(props) {
+{/* Classes */}
+import React, { Component } from "react";
+class Welcome extends React.Component{
+  constructor(props){
     super(props);
   }
-  render() {
-    return <h1>Meet the { this.props.name }</h1>;
-   }
+  render(){
+    return <h1>Meet the <i style={{color:"blueviolet"}}>{this.props.someName}</i></h1>
+  }
 }
-
-const Hello = (props) => {
-  return <h1>Meet the <i>{props.name}</i></h1>
+{/* Hooks */}
+const Hello=(props) =>{
+  return <h1>Meet the <i style={{color:"steelblue"}}>{props.someName}</i></h1>
 }
-
 function App() {
-  const name = 'StarGazers'
+  const someName = 'StarGazers'
   return (
     <div className="container">
       <article>
+        <img src="images/group.svg" alt="StarGazers Group" />
         <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="Stars" />
-          <Hello name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <Welcome someName="StarGazers"/>
+          <Hello someName="StarGazers"/>
+          <p>Members of an <b>intergalactic alliance</b> <br/>
+          paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi There!')}>Click It</button>
         </hgroup>
       </article>
     </div>
