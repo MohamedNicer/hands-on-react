54c3ef677725e2470c702544ec14644af5f0093e
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 17:07:38 +0000

    Refactoring Data

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import ListCast from './components/ListCast';
 import Modals from './components/Modals';
 
 function App() {
-  const name = 'StarGazers'
+  let [memberInfo , setMemberInfo] = useState(null);
   const [cast, setCast] = useState([]);
-  let [memberInfo, setMemberInfo] = useState(null);
 
   async function fetchCast() {
     const response = await fetch('cast.json');
@@ -15,15 +14,17 @@ function App() {
   useEffect(() => {
     fetchCast();
   });
-
+  
   return (
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>Meet the Stargazers</h1>
+        <h1>Meet the <i style={{color:"blueviolet"}}>StarGazers</i></h1>
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
+        <ListCast cast = { cast } onChoice={(info) => setMemberInfo(info)}/>
+        {memberInfo && <Modals member={memberInfo} handleClose={()=>{
+          setMemberInfo(null)
+        }}/> }
       </hgroup>
     </div>
   )
