530f2f7532f92f83a5ae5ee7c44af9971488ee8e
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 06:18:38 +0000

    Using Effects

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 const pageTitle = document.title;
 
-export default () => {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    count && (document.title = `${pageTitle} - ${count}`)
+export default ()=>{
+  const [count,setCount] = useState(0)
+  useEffect(()=>{
+    count && (document.title=`${pageTitle} - ${count}`)
   })
   return (
-    <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support" : `Supported ${count} times`}
+    <button className="outline" onClick={()=> setCount(count+1)}>
+    {count === 0? 'Click To Support': `Supported ${count} times`}  
     </button>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,3 @@
-export default (props) => {
-  return <h1>Meet the <i>{props.name}</i></h1>
+export default (props) =>{
+  return <h1>Meet the <i style={{color:"blueviolet"}}>{props.someName}</i></h1>
 }
