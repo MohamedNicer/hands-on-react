cbc0f2cf9872801e6fc846ad1e5179757463abed
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 04:53:13 +0000

    Templating with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
         <img src="images/group.svg" alt="StarGazers Group" />
         <hgroup>
           <h1>Meet the <i style={{ color: 'blueviolet' }}>{someName}</i></h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <p>Members of an <b>intergalactic alliance</b> <br/>
+          paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={()=> alert('Hi There!')}>Click It</button>
         </hgroup>
       </article>
