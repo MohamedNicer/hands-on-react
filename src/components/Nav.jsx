7d6f2686b1adc1e536b959027bbd1f135e5c35c0
MohamedNicer <74671948+MohamedNicer@users.noreply.github.com>
2023-11-27 17:22:25 +0000

    Building a Navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,27 @@
-export default ({ cast, onChoice }) => {
+export default ({ cast, onChoice }) =>{
   return (
     <nav className="container">
       <ul>
-        <li>
-          <details role="list">
-            <summary aria-haspopup="listbox" role="link">cast</summary>
-            <ul role="listbox">
-              {cast.map(member => (
+        <li><details role="list">
+          <summary aria-haspopup="listbox" role="link">cast</summary>
+          <ul role="listbox">
+            {
+              cast.map(member => (
                 <li key={member.id}>
-                  <a onClick={() => { onChoice(member) }}
-                    data-tooltip={member.name}>{member.name}</a>
+                  <a onClick={()=>{ onChoice(member) }} 
+                  data-tooltip={member.name}>
+                    {member.name}
+                  </a>
                 </li>
-              ))}
-            </ul>
+              ))
+            }
+          </ul>
           </details>
-        </li>
+          </li>
       </ul>
       <ul>
-        <li><a href="#"><img style={{ height: '50px' }} src="images/logo_bug_stargazers.svg" alt="Stargazers Logo" /></a></li>
+        <li><a href="#"><img style={{height:'50px'}} src="images/logo_bug_stargazers.svg" alt="StarGazers Logo" />
+        </a></li>
       </ul>
     </nav>
   )
